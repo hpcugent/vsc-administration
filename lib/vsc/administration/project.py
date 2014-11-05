607bcbc9976d8737fb02d191c15eb1753fa8951a
Andy Georges <itkovian@gmail.com>
2014-11-05 15:08:55 +0100

    Added quota

--- lib/vsc/administration/project.py
+++ lib/vsc/administration/project.py
@@ -18,34 +18,48 @@ This file provides utilities to set up projects on the VSC clusters.
 
 @author: Andy Georges (Ghent University)
 """
+import logging
 import os
+import pwd
 
+from urllib2 import HTTPError
 
+from vsc.accountpage.wrappers import VscAutogroup, VscGroup, VscProjectQuota
 from vsc.config.base import Muk, VscStorage
 from vsc.filesystem.ext import ExtOperations
 from vsc.filesystem.gpfs import GpfsOperations
 from vsc.filesystem.posix import PosixOperations
-from vsc.ldap.entities import VscLdapGroup
 
 
-class MukProject(VscLdapGroup):
-    """Project that will be run on Muk.
+class VscProject(object):
+    def __init__(self, project_id, rest_client):
+        """
+        Initialise.
+        """
+        self.project_id = project_id
+        self.rest_client = rest_client
 
-    - Check if the project has scratch requirements
-    - Set up the project scratch dir and associated quota
-    - Set up credits?
+        # We immediately retrieve this information
+        try:
+            self.project = VscProject(**(rest_client.project[project_id].get()[1]))
+        except HTTPError:
+            logging.error("Cannot get information from the account page")
+            raise
 
-    """
+        self.group = VscAutogroup(**(self.project.group))
 
-    def __init__(self, project_id, storage=None):
+
+class MukProject(VscProject):
+    """Project that will be run on Muk."""
+
+    def __init__(self, project_id, storage=None, rest_client=None):
         """Initialisation.
 
         @type project_id: string
         @param project_id: the unique ID of the project, i.e.,  the LDAP cn entry
         """
-        super(MukProject, self).__init__(project_id)
+        super(MukProject, self).__init__(project_id, rest_client)
 
-        self.project_id = project_id  # since we still do not have a proper project LDAP tree
         self.muk = Muk()
 
         self.ext = ExtOperations()
@@ -57,8 +71,19 @@ class MukProject(VscLdapGroup):
         else:
             self.storage = storage
 
-        # quota are obtained through LDAP
         self.scratch = self.gpfs.get_filesystem_info(self.muk.scratch_name)
+
+        try:
+            all_quota = [VscProjectQuota(**q) for q in rest_client.project[self.project_id].quota.get()[1]]
+        except HTTPError:
+            logging.exception("Unable to retrieve quota information from the accountpage")
+            self.user_scratch_storage = 0
+        else:
+            muk_quota = filter(lambda q: q.storage['name'] == self.muk.storage_name, all_quota)
+            if muk_quota:
+                self.project_scratch_quota = muk_quota[0]['hard']
+            else:
+                self.project_scratch_quota = 250 * 1024 * 1024 * 1024
 
     def _scratch_path(self, mount_point="gpfs"):
         """Determines the path (relative to the scratch mount point)
@@ -73,10 +98,10 @@ class MukProject(VscLdapGroup):
         elif mount_point == "gpfs":
             mount_path = self.storage[self.muk.storage_name].gpfs_mount_point
         else:
-            self.log.raiseException("mount_point (%s) is not login or gpfs" % (mount_point))
+            logging.error("mount_point (%s) is not login or gpfs" % (mount_point))
+            raise Exception("wrong mount point type")
 
         return os.path.join(mount_path, template[0], template[1](self.project_id))
-
 
     def create_scratch_fileset(self):
         """Create a fileset for the VO on the data filesystem.
@@ -90,7 +115,7 @@ class MukProject(VscLdapGroup):
         path = self._scratch_path()
 
         if not self.gpfs.get_fileset_info(filesystem_name, fileset_name):
-            self.log.info("Creating new fileset for project %s on %s with name %s and path %s" % (self.project_id,
+            logging.info("Creating new fileset for project %s on %s with name %s and path %s" % (self.project_id,
                                                                                                   filesystem_name,
                                                                                                   fileset_name,
                                                                                                   path))
@@ -98,19 +123,16 @@ class MukProject(VscLdapGroup):
             self.gpfs.make_dir(base_dir_hierarchy)
             self.gpfs.make_fileset(path, fileset_name)
         else:
-            self.log.info("Fileset %s already exists for project %s ... not creating again." % (fileset_name,
+            logging.info("Fileset %s already exists for project %s ... not creating again." % (fileset_name,
                                                                                                 self.project_id))
-
-        moderators = [m for m in [VscUser(m_) for m_ in self.moderator] if m.status == 'active']
-
         self.gpfs.chmod(0770, path)
 
-        if moderators:
-            self.gpfs.chown(int(moderators[0].uidNumber), int(self.gidNumber), path)
+        if self.project.submitter:
+            self.gpfs.chown(self.project.submitter.vsc_id_number, self.group.vsc_id_number, path)
         else:
-            self.gpfs.chown(pwd.getpwnam('nobody').pw_uid, int(self.gidNumber), path)
+            self.gpfs.chown(pwd.getpwnam('nobody').pw_uid, self.group.vsc_id_number, path)
 
-        self.gpfs.set_fileset_quota(self.scratchQuota, path, fileset_name)
+        self.gpfs.set_fileset_quota(self.project_scratch_quota, path, fileset_name)
 
     def __setattr__(self, name, value):
         """Override the setting of an attribute:
